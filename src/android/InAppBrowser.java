33d8b42074f63993ff7aa0a665aa9c0d61263498
thelight413 <ohrtsadok@gmail.com>
2017-06-29 12:48:44 -0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -186,6 +186,10 @@ public class InAppBrowser extends CordovaPlugin {
                         // load in InAppBrowser
                         else {
                             Log.d(LOG_TAG, "loading in InAppBrowser");
+                               if(!url.equals(homepage)){
+                          Log.d("here",homepage);
+                       main.addView(toolbar);
+                   }
                             result = showWebPage(url, features);
                         }
                     }
@@ -840,10 +844,7 @@ public class InAppBrowser extends CordovaPlugin {
             String newloc = "";
             if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("file:")) {
                 newloc = url;
-                   if(!url.equals(homepage)){
-                          Log.d("here",homepage);
-                       main.addView(toolbar);
-                   }
+                   
                    
             } else if (url.startsWith(WebView.SCHEME_TEL)) {
                 try {
@@ -916,6 +917,11 @@ public class InAppBrowser extends CordovaPlugin {
             String newloc = "";
             if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("file:")) {
                 newloc = url;
+                   if(!url.equals(homepage)){
+                         Log.d("here",homepage);
+                       main.addView(toolbar);
+                   }
+                   Lod.d("onpagestarted",url);
             }
             else
             {
