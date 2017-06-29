593dcb9bbb8f36fb8e2ba4b9afb94a00b9d1856e
thelight413 <ohrtsadok@gmail.com>
2017-06-29 17:08:56 -0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -952,10 +952,10 @@ public class InAppBrowser extends CordovaPlugin {
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
                if(url.equals(homepage)){
-                       String html = "console.log(document.getElementsByClassName('td-header-wrap td-header-style-6'));document.getElementsByClassName('td-header-wrap td-header-style-6')[0].cssText = 'display:block!important;'";
+                       String html = "console.log(document.getElementsByClassName('td-header-wrap td-header-style-6'));document.getElementsByClassName('td-header-wrap td-header-style-6')[0].style.cssText = 'display:block!important'";
                        injectDeferredObject(html,null);
                 }else{
-                     String html = "console.log(document.getElementsByClassName('td-header-wrap td-header-style-6'));document.getElementsByClassName('td-header-wrap td-header-style-6')[0].cssText =  'display:none!important;'";
+                     String html = "console.log(document.getElementsByClassName('td-header-wrap td-header-style-6'));document.getElementsByClassName('td-header-wrap td-header-style-6')[0].style.cssText =  'display:none!important'";
                        injectDeferredObject(html,null);
                 }
           
