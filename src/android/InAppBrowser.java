08effe24a3bc9fd63c7c7ae90ce567962539c2df
thelight413 <ohrtsadok@gmail.com>
2017-06-29 16:56:19 -0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -952,10 +952,10 @@ public class InAppBrowser extends CordovaPlugin {
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
                if(url.equals(homepage)){
-                       String html = "document.getElementsByClassName('td-header-wrap td-header-style-6')[0].display = 'block!important;'";
+                       String html = "document.getElementsByClassName('td-header-wrap td-header-style-6')[0].style.display = 'block!important;'";
                        injectDeferredObject(html,null);
                 }else{
-                     String html = "document.getElementsByClassName('td-header-wrap td-header-style-6')[0].display = 'none!important;'";
+                     String html = "document.getElementsByClassName('td-header-wrap td-header-style-6')[0].style.display = 'none!important;'";
                        injectDeferredObject(html,null);
                 }
           
