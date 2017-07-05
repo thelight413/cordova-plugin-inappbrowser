710090b04b3e087d9c6257de53ded9441f72dc2b
thelight413 <ohrtsadok@gmail.com>
2017-07-05 09:46:35 -0400

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -786,7 +786,30 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+   [super viewDidLoad];
+   // Do any additional setup after loading the view, typically from a nib.
+
+   // Make webView a delegate to itself
+
+   // I am going to add URL information
+   NSString *fullURL = @"http://theyeshivaworld.com/";
+   NSURL *url = [NSURL URLWithString:fullURL];
+   NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+   _webView.delegate = (id)self;
+   [_webView loadRequest:requestObj];
+
+   UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+   [refreshControl addTarget:self action:@selector(handleRefresh:) forControlEvents:UIControlEventValueChanged];
+   [_webView.scrollView addSubview:refreshControl]; //<- this is point to use. Add "scrollView" property.
+}
+
+-(void)handleRefresh:(UIRefreshControl *)refresh {
+   // Reload my data
+   NSString *fullURL = @"http://theyeshivaworld.com/";
+   NSURL *url = [NSURL URLWithString:fullURL];
+   NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+   [_webView loadRequest:requestObj];
+   [refresh endRefreshing];
 }
 
 - (void)viewDidUnload
