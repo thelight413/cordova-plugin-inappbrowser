933db17d80bdc93a0a427033b43f559a73203272
thelight413 <ohrtsadok@gmail.com>
2017-07-05 10:32:14 -0400

    Update CDVInAppBrowser.h

--- src/ios/CDVInAppBrowser.h
+++ src/ios/CDVInAppBrowser.h
@@ -88,6 +88,7 @@
 @property (nonatomic, strong) IBOutlet UILabel* addressLabel;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem* imageButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
 @property (nonatomic, strong) IBOutlet UIToolbar* toolbar;
 @property (nonatomic, strong) IBOutlet UIImage* webimage;
