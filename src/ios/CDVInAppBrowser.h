461f513e5882ce33bb786dcff2352e46b802b736
thelight413 <ohrtsadok@gmail.com>
2017-07-05 10:28:21 -0400

    Update CDVInAppBrowser.h

--- src/ios/CDVInAppBrowser.h
+++ src/ios/CDVInAppBrowser.h
@@ -90,6 +90,7 @@
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
 @property (nonatomic, strong) IBOutlet UIToolbar* toolbar;
+@property (nonatomic, strong) IBOutlet UIImage* webimage;
 
 @property (nonatomic, weak) id <CDVScreenOrientationDelegate> orientationDelegate;
 @property (nonatomic, weak) CDVInAppBrowser* navigationDelegate;
