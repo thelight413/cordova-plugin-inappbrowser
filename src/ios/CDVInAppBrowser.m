7c9d59dd27fc2c74cd6238e5669d1718e11258a9
thelight413 <ohrtsadok@gmail.com>
2017-07-05 12:32:19 -0400

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -646,6 +646,7 @@
     self.backButton.imageInsets = UIEdgeInsetsZero;
 	
     UIImage *webimage = [UIImage imageNamed:@"icon.png"];
+    NSLog(@"%@",webimage);
     self.imageButton = [[UIBarButtonItem alloc] initWithImage:webimage style:UIBarButtonItemStylePlain target:self action:nil];
     [self.toolbar setItems:@[self.backButton,self.imageButton,self.forwardButton]];
 	
