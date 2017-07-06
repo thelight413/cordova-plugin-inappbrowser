fd98b2b9da2d596454866d224323ac77a99d70a4
thelight413 <ohrtsadok@gmail.com>
2017-07-06 09:28:19 -0400

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -655,7 +655,7 @@
         UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
 	 self.imageButton = [[UIBarButtonItem alloc] initWithImage:newImage style:UIBarButtonSystemItemFixedSpace target:self action:nil];
-    [self.toolbar setItems:@[self.backButton,fixedSpaceButton,fixedSpaceButton,self.imageButton,fixedSpaceButton]];
+    [self.toolbar setItems:@[self.backButton,fixedSpaceButton,fixedSpaceButton,fixedSpaceButton,self.imageButton,fixedSpaceButton]];
 	
     self.view.backgroundColor = [UIColor colorWithRed:0.10 green:0.22 blue:0.36 alpha:1.0];
     [self.view addSubview:self.toolbar];
@@ -951,11 +951,11 @@
     NSLog(@"Self.webview:%@",self.webView);
     NSLog(@"REFRESHCONTROL:%@",refreshControl);
     [self.webView.scrollView addSubview:refreshControl];
-    NSLog(@"TOOLBAR:%@",self.toolbar);	
+    NSLog(@"TOOLBAR:%@",self.view);	
     if(!theWebView.canGoBack) {
-    	self.toolbar.hidden = YES;
+    	self.view.hidden = YES;
     }else{
-        self.toolbar.hidden = NO;
+        self.view.hidden = NO;
     }
     [self.spinner stopAnimating];
 
