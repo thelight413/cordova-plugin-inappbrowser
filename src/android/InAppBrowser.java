3614f95c4e85e971c881873390c1f9bfd6fa3308
thelight413 <ohrtsadok@gmail.com>
2017-07-06 15:16:31 -0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -652,7 +652,6 @@ public class InAppBrowser extends CordovaPlugin {
                 icon_button.setBackground(null);
                 icon_button.setImageDrawable(iconIcon);
                 icon_button.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                icon_button.setPadding(0, this.dpToPixels(10), 0, this.dpToPixels(10));
                 icon_button.getAdjustViewBounds();
 
               
