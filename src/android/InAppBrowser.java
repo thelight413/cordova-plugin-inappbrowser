b12ec4c940ba6bc34eedcd8800a6003c05a36836
thelight413 <ohrtsadok@gmail.com>
2017-07-10 12:20:54 -0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -17,7 +17,7 @@
        under the License.
 */
 package org.apache.cordova.inappbrowser;
-
+import org.apache.cordova.inappbrowser.R;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
